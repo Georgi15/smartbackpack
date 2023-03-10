8568f1a7f120ad4d168484bbb5cfe33763008f68
Viktor Alexiev <viktor.i.aleksiev.2022@elsys-bg.org>
2023-03-10 15:41:44 +0200

    added pincode

--- smart_backpack.ino
+++ smart_backpack.ino
@@ -3,6 +3,20 @@
 #include  <SoftwareSerial.h>
 #include "BluetoothSerial.h"
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+const int rows = 4;
+const int cols = 4;
+char keys[rows][cols] = {
+  {'1', '2', '3', 'U'},
+  {'4', '5', '6', 'D'},
+  {'7', '8', '9', 'C'},
+  {'L', '0', 'R', 'E'}
+};
+int row_pins[rows] = {16, 4, 2, 15};
+int col_pins[cols] = {23, 19, 18, 5};
+
+Keypad my_keypad = Keypad(makeKeymap(keys), row_pins, col_pins, rows, cols);
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -11,9 +25,12 @@
 BluetoothSerial SerialBT;
 
 bool device_connected = false;
+bool unlocked = false;
 String data_string = "";
 String data_buf;
 String incoming_data;
+String entered_pin = "";
+String pin = 1234;
 
 #define dhttype 11
 #define dht_pin 33
@@ -28,7 +45,7 @@ TinyGPSPlus gps;
 
 SoftwareSerial ss(RXPin, TXPin);
 
-LiquidCeystal_I2C lcd(0x27, 16, 2)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(dht_pin, dhttype);
 
 void setup() {
@@ -121,6 +138,34 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Enter pincode: ");
+  lcd.setCursor(0, 2);
+  while(unlocked == false)
+  {
+    char key = my_keypad.getKey();
+    entered_pin += key;
+    if(key == '\n' && entered_pin == pin)
+    {
+      unlocked = true;
+      break;
+    }
+    else
+    if(key == '\n' && entered_pin!=pin)
+    {
+      lcd.print("Enter pincode: ");
+      lcd.setCursor(0, 2);
+    }
+    lcd.print(entered_pin);
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if(unlocked == true)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("air_hum is " + String(air_h));
+    lcd.setCursor(0, 2);
+    lcd.print("air_temp is " + String(air_t));
+  }
+  
   
 
 
